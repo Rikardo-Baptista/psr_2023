6e9b44605dbfb0acd546b8a4733d0c5f7060ec40
Rikardo-Baptista <ricardo.baptista@ua.pt>
2023-09-28 11:38:24 +0100

    Ex numero complexos

---
+++ Ex2/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#shebang line to inform the OS that the content is in python
+
+#use imports here
+from colorama import Fore, Back, Style
+from time import time
+import math
+
+# define functions here ...
+def addComplex(x, y):
+    x + y 
+
+
+def main():
+
+if __name__ == '__main__':
+    main()
